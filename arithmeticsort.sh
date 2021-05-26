9d5f0eafcf131dd4cbcfeba20f2933d313e0ccfd
balu-pinisetty <balupinisetty@gmail.com>
2021-05-26 13:26:05 +0530

    Reading the User inputs

--- arithmeticsort.sh
+++ arithmeticsort.sh
@@ -1,2 +1,8 @@
 #!/bin/bash -x
 echo "Welocome to Arithmetic Computation & Sorting Problem"
+
+read -p "Enter the 1st value: " a
+read -p "Enter the 2nd value: " b
+read -p "Enter the 3rd value: " c
+
+echo "User inputs are $a $b & $c"
