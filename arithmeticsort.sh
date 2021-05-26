2b820874a7620c8a57116247bcf000a2c48324e2
balu-pinisetty <balupinisetty@gmail.com>
2021-05-26 13:54:24 +0530

    Computing a+b/c

--- arithmeticsort.sh
+++ arithmeticsort.sh
@@ -5,6 +5,7 @@ read -p "Enter the 1st value: " a
 read -p "Enter the 2nd value: " b
 read -p "Enter the 3rd value: " c
 
-echo -e "User inputs are\na=$a\nb=$b\nc=$c\ncomputing a*b+c"
+echo -e "User inputs are\na=$a\nb=$b\nc=$c\ncomputing a+b/c"
 
-echo "Result=$(($a*$b+$c))"
+Result=$( perl -e "{ print ($a+$b/$c)}")
+echo "Result=$Result"
