92d5175846108b86b8c87be2c23d12e358ec2430
balu-pinisetty <balupinisetty@gmail.com>
2021-05-26 14:36:18 +0530

    Storing result values in Array

--- arithmeticsort.sh
+++ arithmeticsort.sh
@@ -9,7 +9,14 @@ dict[a+b*c]=$(($a+$b*$c))
 dict[a*b+c]=$(($a*$b+$c))
 dict[a+b/c]=$(perl -e "{ print ($a+$b/$c) }")
 dict[a%b+c]=$(($a%$b+$c))
+i=0
 for key in ${!dict[@]}
 do
 echo $key=${dict[$key]}
+array[i++]=${dict[$key]}
 done
+for i in ${!array[@]}
+do
+echo "Result[$i]=${array[$i]}"
+done
+
