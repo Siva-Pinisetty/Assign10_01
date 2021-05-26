6b29f2b6033c220d7071bc3a22475586930e4a8d
balu-pinisetty <balupinisetty@gmail.com>
2021-05-26 15:47:41 +0530

    Sorting Array in descending

--- arithmeticsort.sh
+++ arithmeticsort.sh
@@ -7,7 +7,7 @@ read -p "Enter the 3rd value: " c
 echo -e "User inputs are\na=$a\nb=$b\nc=$c\nComputing Operations:"
 dict[a+b*c]=$(($a+$b*$c))
 dict[a*b+c]=$(($a*$b+$c))
-dict[a+b/c]=$(perl -e "{ print ($a+$b/$c) }")
+dict[a+b/c]=$(($a+$b/$c))
 dict[a%b+c]=$(($a%$b+$c))
 i=0
 for key in ${!dict[@]}
@@ -15,8 +15,17 @@ do
 echo $key=${dict[$key]}
 array[i++]=${dict[$key]}
 done
-for i in ${!array[@]}
+echo ${array[@]}
+for ((i=1; i<4; i++))
 do
-echo "Result[$i]=${array[$i]}"
+  for ((j=0; j<i; j++))
+  do
+    if [ ${array[$i]} -gt ${array[$j]} ]
+    then
+	temp=${array[$j]}
+	array[$j]=${array[$i]}
+	array[$i]=$temp
+    fi
+  done
 done
-
+echo -e "Descending order of values\n${array[@]}"
